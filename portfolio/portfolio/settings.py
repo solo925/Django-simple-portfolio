de98aa8cc8ebe03dde662d6899055c929371352d
davinci <davinci@localhost.localdomain>
2024-06-08 10:17:14 +0300

    merge

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-da%1qpjcou=#3bhy6(2oheqixuc36ftjbv!3lyf3bo5cynh($u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
