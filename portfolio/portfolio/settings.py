5281d3945e20468685d1edc7bfd13f819cc502e5
davinci <davinci@localhost.localdomain>
2024-06-11 09:34:04 +0300

    add host

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-da%1qpjcou=#3bhy6(2oheqixuc36ftjbv!3lyf3bo5cynh($u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
